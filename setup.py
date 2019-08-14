bd97bf398aafbbe0ec221352544bba04fd681b66
Tiago Ribeiro <tiago.astro@gmail.com>
2019-08-13 23:55:53 -0700

    Add package data path to idl files.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     # install_requires=install_requires,
     package_dir={"": "python"},
     packages=find_namespace_packages(where="python"),
-    package_data={"": ["*.rst", "*.yaml", "*.idl", "*.xml"]},
+    package_data={"": ["*.rst", "*.yaml", "idl/*", "*.xml"]},
     include_package_data=True,
     # scripts=[],
     # tests_require=tests_require,
